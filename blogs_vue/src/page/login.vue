d9d27176e5ffd8e559a6935bcab7d497cbd6f4d5
澜立齐 <1532900523@qq.com>
2019-06-04 17:27:40 +0800

    临时提交

---
+++ blogs_vue/src/page/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-form label-width="80px">
+      <el-form-item label="账号">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button>登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {}
+};
+</script>
+<style>
+</style>
